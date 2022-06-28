93dbc8b512ae70a36b3eaf85431db587cdd4db9e
TheSheridanProgrammer <sycratech@gmail.com>
2022-06-28 18:19:53 -0400

    Add project files.

---
+++ AS_Runtime_Engine/CoreEngine.cs
@@ -0,0 +1,44 @@
+﻿namespace AS_Runtime_Engine
+{
+    public class CoreEngine
+    {
+        private static List<Variable> _variables;
+        static CoreEngine()
+        {
+            // Check if static needed
+            _variables = new List<Variable>();
+        }
+        public static string Eval(string term1, string term2, char op)
+        {
+            switch (op)
+            {
+                case '+':
+                    return (int.Parse(term1) + int.Parse(term2)).ToString();
+                case '-':
+                    return (int.Parse(term1) + int.Parse(term2)).ToString();
+                case '*':
+                    return (int.Parse(term1) + int.Parse(term2)).ToString();
+                case '/':
+                    return (int.Parse(term1) + int.Parse(term2)).ToString();
+                default:
+                    return "Operation Error";
+            }
+        }
+
+        public static string MakeOrUpdateVar(string name, string value)
+        {
+            // If variable exists, update the value
+            foreach (Variable variable in _variables)
+            {
+                if (variable.Name == name)
+                {
+                    variable.Value = value;
+                    return "Variable Updated";
+                }
+            }
+            // If variable did not exist then create a new one
+            _variables.Add(new Variable(name, value));
+            return "Variable Created";
+        }
+    }
+}
---
+++ AS_Runtime_Engine/Variable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AS_Runtime_Engine
+{
+    internal class Variable
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+
+        public Variable(string name, string value)
+        {
+            Name = name;
+            Value = value;
+        }
+    }
+}
